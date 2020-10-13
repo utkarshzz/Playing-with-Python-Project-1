11d98e9ba0760955ea56a12ea0c3d4424703cba1
Ishaarora1234 <65450919+Ishaarora1234@users.noreply.github.com>
2020-10-13 19:11:51 +0530

    Update Text_to_speech (1).py

--- Day-1/Isha_Arora/Text_to_speech (1).py
+++ Day-1/Isha_Arora/Text_to_speech (1).py
@@ -13,18 +13,6 @@ engine.runAndWait()
 # In[5]:
 
 
-get_ipython().system('pip install gTTS')
-get_ipython().system('pip install playsound')
-from gtts import gTTS
-#playsound pkg is used to get any sound data from Gtts and save it in a sound file
-from playsound import playsound
-text_val = 'Hello world out there'
-language ='en'
-obj = gTTS(text = text_val , lang = language , slow= False)
-obj.save("exams.mp3")
-playsound("exams.mp3")
-
-
 # In[ ]:
 
 
