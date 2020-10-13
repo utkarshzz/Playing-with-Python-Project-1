d3b66596cb25495e71ea4db4933589154618f809
Chiranjib Bordoloi <chiranjib.bordoloi30@gmail.com>
2020-10-13 17:51:20 +0530

    Updated

--- Day-1/Chiranjib_Bordoloi/Integral.py
+++ Day-1/Chiranjib_Bordoloi/Integral.py
@@ -2,17 +2,15 @@
 # pip install sympy
 
 # Importing sympy
-from sympy import *
+import sympy as sp
 
-init_printing(use_unicode=True)
-
-x = Symbol('x')
+x = sp.symbols('x')
 
 # Expressions
-exp1 = x * log(x)
+exp1 = x * sp.log(x)
 exp2 = x ** 2
-exp3 = tan(sin(x))
+exp3 = sp.tan(sp.sin(x))
 
-print("Integration of", exp1, "is", integrate(exp1, x))
-print("Integration of", exp2, "is", integrate(exp2, x))
-print("Integration of", exp3, "is", integrate(exp3, x))
+print("Integration of", exp1, "is", sp.integrate(exp1, x))
+print("Integration of", exp2, "is", sp.integrate(exp2, x))
+print("Integration of", exp3, "is", sp.integrate(exp3, x))
--- Day-1/Chiranjib_Bordoloi/TemperatureConversion.py
+++ Day-1/Chiranjib_Bordoloi/TemperatureConversion.py
@@ -1,26 +1,28 @@
-#Function to convert Celsius to Fahrenheit
+# Function to convert Celsius to Fahrenheit
 def C_to_F():
-  cel = float(input("Enter temperature in Celsius: "))
-  fah = (cel * 9/5) + 32
-  print(cel,"°C =",fah,"F")
+    cel = float(input("Enter temperature in Celsius: "))
+    fah = (cel * 9 / 5) + 32
+    print(cel, "°C =", fah, "F")
 
-#Function to convert Fahrenheit to Celsius
+
+# Function to convert Fahrenheit to Celsius
 def F_to_C():
-  fah = float(input("Enter temperature in fahrenheit: "))
-  cel = (fah - 32) * 5/9
-  print(fah,"F =",cel,"°C")
+    fah = float(input("Enter temperature in fahrenheit: "))
+    cel = (fah - 32) * 5 / 9
+    print(fah, "F =", cel, "°C")
 
-#Menu for selection
+
+# Menu for selection
 print("1. Celcius to Farenheit")
 print("2. Farenheit to Celcius")
 
 choice = int(input("Enter a Selection: "))
 
-if (choice==1):
-  print("\nCelcius to Farenheit\n")
-  C_to_F()
-elif (choice==2):
-  print("\nFarenheit to Celcius\n")
-  F_to_C()
+if (choice == 1):
+    print("\nCelcius to Farenheit\n")
+    C_to_F()
+elif (choice == 2):
+    print("\nFarenheit to Celcius\n")
+    F_to_C()
 else:
-  print("Invalid Selection!!")
+    print("Invalid Selection!!")
