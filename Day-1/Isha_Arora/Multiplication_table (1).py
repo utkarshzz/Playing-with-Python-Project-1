67e391c530b434aa30dcb48428b28004e42e9179
Ishaarora1234 <65450919+Ishaarora1234@users.noreply.github.com>
2020-10-13 19:52:16 +0530

    Update Multiplication_table (1).py

--- Day-1/Isha_Arora/Multiplication_table (1).py
+++ Day-1/Isha_Arora/Multiplication_table (1).py
@@ -8,9 +8,8 @@ def main() :
     num = int(input("enter a number :"))
     for i in range(1, 11) :
         print(num * i)
-        
-        
+
+
 if __name__ == "__main__" :
     main()
-    
-    
+
