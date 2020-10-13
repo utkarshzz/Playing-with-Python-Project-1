4a4340d1eed8e3541c6c54f519e3951f10428030
Ishaarora1234 <65450919+Ishaarora1234@users.noreply.github.com>
2020-10-13 19:09:36 +0530

    Update Find_Integral (1).py

--- Day-1/Isha_Arora/Find_Integral (1).py
+++ Day-1/Isha_Arora/Find_Integral (1).py
@@ -6,14 +6,14 @@
 
 import sympy as sp
 
-def main():
-    x=sp.symbols('x')
-    i1=sp.integrate(sp.log(x)*x,x)
-    i2=sp.integrate(x**2,x)
-    i3=sp.integrate(sp.tan(sp.sin(x)),x)
-    print(i1)
-    print(i2)
-    print(i3)
+def main() :
+    x = sp.symbols( ' x ' )
+    i1 = sp.integrate( sp.log(x) * x, x )
+    i2 = sp.integrate( x** 2, x )
+    i3 = sp.integrate( sp.tan( sp.sin(x) ), x )
+    print( i1 )
+    print( i2 )
+    print( i3 )
 if __name__ == "__main__" :
     main()
 
