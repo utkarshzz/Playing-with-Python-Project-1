a75634ea65047fcb7ef760da5dc7f65571fab19c
gyan-krishna <45621173+gyan-krishna@users.noreply.github.com>
2020-10-13 12:28:26 +0530

    submission of day1

---
+++ Day-1/ConvertTemperatures.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Python code to change temperatures :
+From Celcius to Farenhite & ViceVersa
+Input your temperature
+and change itCreated on Tue Oct 13 09:45:41 2020
+@author: Gyan Krishna
+"""
+
+def main():
+    temp = input("enter the temperature")
+    l = int(len(temp))
+    postfix = temp[l-1] 
+
+    if postfix == 'C' or postfix == 'c':
+        
+        celcius = int(temp[0:l-1])
+        farenheit = 9/5*celcius + 32
+        
+        print("Temp in farenheit is :: ",farenheit)
+    elif postfix == 'F' or postfix == 'f':
+        
+        farenheit = int(temp[0:l-1])
+        celcius = 5/9*(farenheit - 32)
+        
+        print("Temp in Celcius is :: ",celcius)
+    else:
+        print("input format error!")
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ Day-1/FindIntegral.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Python Codes to find Integrals of the following function :
+1.) xlog(x)
+2.) x^2
+3.) tan(sin(x))
+
+Created on Tue Oct 13 10:08:37 2020
+@author: Gyan Krishna
+
+install the following packages:-
+pip install sympy
+"""
+
+from sympy import *
+
+def main():
+    x = symbols('x')
+    
+    form1 = x * log(x)
+    form2 = x**2
+    form3 = tan( sin(x) )
+    
+    intr1 = integrate(form1,x)
+    intr2 = integrate(form2,x)
+    intr3 = integrate(form3,x)
+    
+    print("intrgration of ",form1,"is :: ",intr1)
+    print("intrgration of ",form2,"is :: ",intr2)
+    print("intrgration of ",form3,"is :: ",intr3)
+    
+if __name__ == "__main__":
+    main()
+    
